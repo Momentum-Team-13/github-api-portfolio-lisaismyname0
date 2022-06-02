2827163087f2f358696a4e29761671d97021fcb2
Lisa Williams <lisaismyname0@gmail.com>
2022-06-02 17:49:18 -0400

    list of repos appear in div

--- main.js
+++ main.js
@@ -19,10 +19,8 @@ fetch(gitHubUrl, {
         buildProfile(data);
         // let profileData = data
 
-        // building html header portion to include photo, name, and brief profile
+        // buildRepo(data);
     })
-
-// for this data we need to add to the page by building new elements and including data from a database
 
 function buildProfile(profileData) {
     console.log(profileData)
@@ -60,36 +58,56 @@ function buildProfile(profileData) {
     userNameElement.classList.add("username")
     userNameElement.innerText = `GitHub Username: ${profileData.login}`
     nameElement.appendChild(userNameElement)
-
-    // separate element for repos
-    let repoElement = document.createElement("p")
-    repoElement.classList.add("repos")
-    repoElement.innerText = "Repositories"
-    containerElement.appendChild(repoElement)
-
-    let repo1 = document.createElement("li")
-    repo1.innerText = "repo #1"
-    let repo2 = document.createElement("li")
-    let repo3 = document.createElement("li")
-    repo2.innerText = "repo #2"
-    repo3.innerText = "repo #3"
-    repoElement.appendChild(repo1)
-    // repoElement.appendChild(repo2)
-    // repoElement.appendChild(repo3)
-    // is there a way to do this all in one? as in repo.Element.appendChild(repo1,repo2,repo3)
-
 }
 
 let repoUrl = "https://api.github.com/users/lisaismyname0/repos"
-// do we need to do a separate fetch request for this? no because the personal information is in the repo information & technically less API requests means faster but it might be easier to do it separately 
+// do we need to do a separate fetch request for this? no because the personal information is in the repo information & technically less API requests means faster but it might be easier to do it separately
+
+fetch(repoUrl, {
+    method: "GET",
+    headers: { "Content-Type": "application.json" }
+})
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        buildRepo(data)
+    })
+
+function buildRepo(repoData) {
+    for (let repo of repoData) {
+        const repoInfo = document.querySelector(".repos")
+        const repoElement = document.createElement("div")
+        repoElement.innerText = `${repo.name}`
+        repoInfo.appendChild(repoElement)
+        console.log(repoInfo)
+    }
+}
+// separate element for repos
+// repoElement.innerText = "Repositories"
+// containerElement.appendChild(repoElement)
+// buildRepo(data)
+
+// function buildRepos(profile)
+
+// let repo1 = document.createElement("li")
+// repo1.innerText = "repo #1"
+// let repo2 = document.createElement("li")
+// let repo3 = document.createElement("li")
+// repo2.innerText = "repo #2"
+// repo3.innerText = "repo #3"
+// repoElement.appendChild(repo1)
+// repoElement.appendChild(repo2)
+// repoElement.appendChild(repo3)
+// is there a way to do this all in one? as in repo.Element.appendChild(repo1,repo2,repo3) look into append()
 
 // can trigger the fetch with an event listener
 
-function buildProfile(profileData) {
-    profileData.map(function (repo) {
-        profile.appendChild(buildRepoElement(repo.name))
-    })
-}
+// function buildProfile(profileData) {
+//     profileData.map(function (repo) {
+//         profile.appendChild(buildRepoElement(repo.name))
+//     })
+// }
 
 // ^ when profileData is linked to the repos, this cycles through the list of repos and returns only the information asked for... this is also in map format which is a more streamlined way of writing this loop:
 
@@ -101,12 +119,5 @@ function buildProfile(profileData) {
 //     }
 // }
 
-// function buildRepoElement(name) {
-//     let el = document.createElement("p")
-//     el.innerText = name
-//     return el
-// returns new element for a repo, like a customer
-// }
 
-// Think about streamlining functions with map instead of appendChild()
-
+// Think about streamlining functions with map instead of loops
